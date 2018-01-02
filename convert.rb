928cc7969551a1da3f5095d904edd2e0b5479277
joey <jiangjun12321@sina.com>
2018-01-02 19:01:20 +0800

    optimize

--- convert.rb
+++ convert.rb
@@ -16,12 +16,14 @@ class Array
 end
 
 class Convertion
-  attr_accessor :camel_names, :columns, :model_name, :is_list, :undersocre_names, :types
+  attr_accessor :camel_names, :columns, :model_name, :is_list, :undersocre_names, :types, :use_symbol
 
   def initialize(options={})
 
    @camel_names = options[:camel_names]
-
+   
+   @use_symbol = options[:use_symbol]
+   @use_symbol = true if @use_symbol.nil?
    @undersocre_names = @camel_names.map{|i|i.underscore}
 
    @types = options[:types].polish_types
@@ -42,7 +44,8 @@ class Convertion
 
     puts "\n\n==================== MIGRATE CODE IS ====================\n\n"
     puts "create_#{model_name.underscore}:"
-    puts "rails g model #{model_name} user:references \\"
+    puts "rails g model #{model_name} user:references loan_id:integer json_data:text \\"
+
     if @undersocre_names.size == types.size
       undersocre_names.zip(types).each{|n,t| puts "#{n}:#{t} \\"}
     end   
@@ -50,13 +53,15 @@ class Convertion
 
   def list_code
     return <<-EFO
-def self.my_create(user_id, data)
+def self.my_create(user_id, data, loan_id)
   return if data.blank?
   columns = (column_names.select{|x| !["id", "created_at", "updated_at"].include? x}).map {|x|x.to_sym}
   array = []
   data.each do |sub_data|
     array << {
-      :user_id=> user_id,  
+      :user_id => user_id,
+      :loan_id => loan_id,  
+      :json_data => data[:json_data],
 #{attribute_string}  
     }
   end
@@ -68,9 +73,11 @@ end
 
   def single_code
     return <<-EFO
-def self.my_create(user_id, data)
+def self.my_create(user_id, data, loan_id)
   attribute = {
       :user_id=> user_id,  
+      :loan_id => loan_id, 
+      :json_data => data[:json_data],
 #{attribute_string}  
   }
 
@@ -88,13 +95,75 @@ end
     return str
   end
 
-  def parse_by_type(type, n)
-    prefix = @is_list ? "sub_data[\"#{n}\"]" : "data[\"#{n}\"]"
+  def parse_by_type(type, n, use_symbol=false)
+    # use_symbol = true
+    if @use_symbol
+      prefix = @is_list ? "sub_data[:#{n}]" : "data[:#{n}]"
+    else 
+      prefix = @is_list ? "sub_data[\"#{n}\"]" : "data[\"#{n}\"]" 
+    end
     case type
-    when 'string','integer','float' then prefix
+    when 'string' then prefix
+    when 'integer','float','boolean' then "type_convert(#{prefix}, '#{type}')"
     when 'datetime' then "(#{prefix}.to_datetime rescue nil)"  
     when 'text' then "(#{prefix}.to_json rescue nil)"  
+    else "parse_by_type 未知类型"
     end
   end
+end
+
+class ConvertionRaw < Convertion
+  def convert
+    code_string = @is_list ? list_code : single_code
+
+    relation_string = "has_many :#{model_name.underscore.pluralize}, class_name: 'Warehouse::#{model_name}', dependent: :destroy"
+    puts "\n\n==================== RELATION CODE IS ====================\n\n"
+    puts relation_string
+
+    puts "\n\n==================== CREATE CODE IS ====================\n\n"
+    puts code_string
+
+    puts "\n\n==================== MIGRATE CODE IS ====================\n\n"
+    puts "create_#{model_name.underscore}:"
+    puts "rails g model #{model_name} user:references loan_id:integer\\"
+
+    if @undersocre_names.size == types.size
+      undersocre_names.zip(types).each{|n,t| puts "#{n}:#{t} \\"}
+    end   
+  end
+
+  def list_code
+    return <<-EFO
+def self.my_create(user_id, data, loan_id)
+  return if data.blank?
+  columns = (column_names.select{|x| !["id", "created_at", "updated_at"].include? x}).map {|x|x.to_sym}
+  array = []
+  data.each do |sub_data|
+    array << {
+      :user_id => user_id,
+      :loan_id => loan_id,  
+#{attribute_string}  
+    }
+  end
+
+  import columns, array, :validate => false
+end  
+    EFO
+  end
+
+  def single_code
+    return <<-EFO
+def self.my_create(user_id, data, loan_id)
+  attribute = {
+      :user_id=> user_id,  
+      :loan_id => loan_id, 
+#{attribute_string}  
+  }
+
+  create!(attribute) 
+end  
+    EFO
+  end
+
 end
 
---
+++ lib/utils.rb
@@ -0,0 +1,66 @@
+class String
+  # 大于 9 位的数字应该认为是编号
+  def is_integer?
+    self.to_i.to_s == self && self.to_s.size < 9
+  end
+
+  def is_float?
+    self.to_f.to_s == self
+  end
+end
+module Utils
+  def convert_type(data)
+    types = data.values.map do |e|
+      raise RuntimeError.new('未知数据类型') if e.to_s.downcase == 'null'
+      case e.class.to_s
+      when 'String' then 'string'
+      when 'Fixnum' then 'integer'
+      when 'Float' then 'float'
+      when 'TrueClass','FalseClass' then 'boolean'  
+      when 'Hash' then 'text' 
+      when 'Date' then 'datetime' 
+      else 
+        p e.class.to_s + "未知"
+      end
+    end
+
+    return types
+  end
+
+  # 该方法会检查字符串类型究竟是 float, datetime, 
+  def strong_convert_type(data)
+    types = data.values.map do |e|
+      raise RuntimeError.new('未知数据类型') if e == 'null'
+      if e.class.to_s == 'String'
+        type_class = check_string(e)
+      else 
+        type_class = e.class.to_s
+      end
+      case type_class
+      when 'String' then 'string'
+      when 'Fixnum' then 'integer'
+      when 'Float' then 'float'
+      when 'TrueClass','FalseClass' then 'boolean'  
+      when 'Hash' then 'text' 
+      when 'Date' then 'datetime' 
+      else 
+        p e.class.to_s + "未知"
+      end
+    end
+  end
+
+  def check_string(str)
+    return 'Fixnum' if str.is_integer? 
+    return 'Float' if str.is_float? 
+    return 'TrueClass' if str == 'true'
+    return 'FalseClass' if str == 'false'
+
+    require 'date'
+    begin
+      return 'Date' if Date.parse(str)
+    rescue ArgumentError
+      return 'String'
+    end
+  end
+
+end
